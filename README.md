0feb6fcf040186cec9ed893a393258a00a4c3db0
WasteOfADrumBum <jmsmall89@gmail.com>
2020-09-12 13:14:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node Express Handlebars
+ Eat-Da-Burger! is a restaurant app that lets users input the names of burgers they'd like to eat.
---
+++ server.js
@@ -0,0 +1,26 @@
+// Require Express
+const express = require("express");
+// Allows Acesses to JSON Body from POST
+const bodyParser = require("body-parser");
+// Require Handlebars
+const exphbs = require("express-handlebars");
+// Initalise Express Server
+const app = express();
+
+// Set Default Port for Express and Heroku
+let PORT = process.env.PORT || 9080;
+// Add Additional Functionality to Express Using Middleware body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+// Serve Static from the "public" Directory
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+
+/* Start Server */
+app.listen(PORT, function () {
+	console.log("App listening on PORT " + PORT);
+});
