60507c88423b12c6bc880ab4aefbf9e8f0cc6933
WasteOfADrumBum <jmsmall89@gmail.com>
2020-09-20 19:56:42 -0400

    REMOVED FRONTEND JS
    NOT NEEDED

--- public/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-	$(".delete-burger").on("click", (event) => {
-		// event.preventDefault();
-		console.log(event.currentTarget);
-		// AJAX pull data .attr method:delete
-	});
-});
