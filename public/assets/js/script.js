667d3f908f6fb5d1f558ba0b5188b39dd6226514
WasteOfADrumBum <jmsmall89@gmail.com>
2020-09-22 12:40:29 -0400

    MISSING CLOSING SYNTAX

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,14 @@
-$(document).ready(function() {
-  $(".delete-burger").on("click", function(event) {
-    event.preventDefault();
-    var burger_id = $(this).data("id");
-    console.log(burger_id);
-    $.ajax({
-      method: "DELETE",
-      url: "/api/burger/" + burger_id
-    }).then(function(data) {
-      // reload page to display updated data
-      location.reload();
-    });
-  });
+$(document).ready(function () {
+	$(".delete-burger").on("click", function (event) {
+		event.preventDefault();
+		var burger_id = $(this).data("id");
+		console.log(burger_id);
+		$.ajax({
+			method: "DELETE",
+			url: "/api/burger/" + burger_id,
+		}).then(function (data) {
+			// reload page to display updated data
+			location.reload();
+		});
+	});
+});
